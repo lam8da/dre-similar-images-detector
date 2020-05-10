e93db68175d1cee2e05c0bc57b150da46f2185d4
lambda <2524872+lam8da@users.noreply.github.com>
2020-05-10 12:25:12 -0700

    add an option to decide whether to rebuild

--- install-run.sh
+++ install-run.sh
@@ -4,16 +4,24 @@ set -ex
 DIR=distribution/dre-1.0.2-macosx
 BINARY=dre.app.macosx.x64.jar 
 
+rebuild=''
+
 build() {
-  # if test -e $DIR/$BINARY; then return; fi
+  if [[ -e $DIR/$BINARY ]] && [[ "$rebuild" != 'yes' ]]; then
+    return
+  fi
   ant make-all-distribution
   # ant clean
 }
 run() {
   cd $DIR
-  rm stdout stderr
-  java -d64 -XstartOnFirstThread -jar $BINARY
+  rm -f stdout stderr
+  # java -d64 -XstartOnFirstThread -jar $BINARY
+  java -XstartOnFirstThread -jar $BINARY
 }
 
+if [[ "$1" == '-f' ]]; then
+  rebuild=yes
+fi
 build
 run
